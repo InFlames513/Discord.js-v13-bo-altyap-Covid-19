970be2efb6b5246ccc2a5df0416ad97293a13b6e
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-03-06 20:11:18 +0300

    Bağımlılıklar yeniden yazıldı ve tüm dosyalar ayarlandı

--- Covid-19 d.js v13/covid.js
+++ Covid-19 d.js v13/covid.js
@@ -1,42 +1,36 @@
 //modüller baş
 const fs = require("fs");
-const Discord = require("discord.js");  //https://discord.gg/HFhyNaKUB6
-const discord = require("discord.js");
-const { Intents } = require("discord.js");
-const client = new discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }); 
+const { Client, Intents, Collection } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 require("./util/eventLoader.js")(client);
-const ayarlar = require("./ayarlar.json");
-const db = require('inflames.db')
+const { token } = require("./ayarlar.json");
 //modüller son https://discord.gg/HFhyNaKUB6
+
 //log baş https://discord.gg/HFhyNaKUB6
-client.once("ready", () => {
-	console.log(`Başarıyla aktif oldum`);
-});
+const log = message => console.log(`${message}`);
 //log son
+
 //command handler baş
-client.once("ready", () => {
-	console.log(`Logged in as @${client.user.tag}!`);
-});
 
-const log = message => {
-	console.log(` ${message}`);
-};
+client.commands = new Collection();
+client.aliases = new Collection();
 
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
 fs.readdir("./komutlar/", (err, files) => {
 	if (err) console.error(err); //https://discord.gg/HFhyNaKUB6
-	log(`${files.length} komut yüklenecek.`);
+
+	log(files.length, "komut yüklenecek.");
+
 	files.forEach(f => {
 		let props = require(`./komutlar/${f}`);
 		log(`Yüklenen komut: ${props.help.name}.`);
 		client.commands.set(props.help.name, props);
-		props.conf.aliases.forEach(alias => {
-			client.aliases.set(alias, props.help.name);
-		});
+		props.conf.aliases.forEach(alias =>
+			client.aliases.set(alias, props.help.name)
+		);
 	});
 });
-       //https://discord.gg/HFhyNaKUB6                              
+
+//https://discord.gg/HFhyNaKUB6                              
 client.reload = command => {  //https://discord.gg/HFhyNaKUB6
 	return new Promise((resolve, reject) => {
 		try {
@@ -88,15 +82,11 @@ client.unload = command => {
 	});
 };
 //command handler son
-//bot oynuyor baş
-client.on("ready", () => {
-	client.user.setActivity(`Yakında Yok Olacağım :D`);
-	console.log("Botun durumu ayarlandı.");
-}); //InFlames#2005
-//bot oynuyor son
+
 //token baş
-client.login(ayarlar.token);
+client.login(token);
 //token son
+
 //ayrıntılı hata baş
 process.on('warning', e => console.warn(e.stack));
 //ayrıntılı hata son
--- Covid-19 d.js v13/events/message.js
+++ Covid-19 d.js v13/events/message.js
@@ -1,22 +1,22 @@
-const fs = require("fs");
-const ayarlar = require("../ayarlar.json");
-const db =  require('inflames.db');
-const Discord = require("discord.js");
- module.exports = async message => {
-var prefix = ayarlar.prefix
-       let client = message.client;
-  if (message.author.bot) message.channel.sendreturn;
-   if (!message.content.startsWith(prefix)) return;
-   let command = message.content.split(" ")[0].slice(prefix.length);
+const { prefix } = require("../ayarlar.json");
 
-   let params = message.content.split(" ").slice(1);
-   let cmd;
-   if (client.commands.has(command)) {
-     cmd = client.commands.get(command);
- } else if (client.aliases.has(command)) {
-     cmd = client.commands.get(client.aliases.get(command));
-   }
- if (cmd) {
-     cmd.run(client, message, params);
- }
-}; 
+module.exports = async message => {
+
+  let client = message.client;
+
+  if (message.author.bot || !message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let params = message.content.split(" ").slice(1);
+
+  let cmd;
+
+  if (client.commands.has(command))
+    cmd = client.commands.get(command);
+  else if (client.aliases.has(command))
+    cmd = client.commands.get(client.aliases.get(command));
+
+  if (cmd)
+    cmd.run(client, message, params);
+
+};
