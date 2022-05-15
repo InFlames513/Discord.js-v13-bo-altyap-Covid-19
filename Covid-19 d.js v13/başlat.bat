790045b31e26fdf62964fcb1e4a62a4f1b648f8c
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-05-15 10:42:00 +0300

    Delete Covid-19 d.js v13 directory

--- Covid-19 d.js v13/başlat.bat
+++
@@ -1,6 +0,0 @@
-echo off
-title Covid-19 bot aktif
-color c
-cls
-:a
-node bot.js
--- Covid-19 d.js v13/util/eventLoader.js
+++
@@ -1,6 +0,0 @@
-const reqEvent = event => require(`../events/${event}`);
-
-module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));
-  client.on('messageCreate', reqEvent('message'));
-};
